f8895693b6362dc429de8cf6d6b74e7c1bd6c49c
jchan3990 <jonchan3990@gmail.com>
2020-11-16 00:53:02 -0800

    Fixed server issue

--- server/index.js
+++ server/index.js
@@ -1,7 +1,7 @@
 const Parser = require('body-parser');
 const express = require('express');
 const app = express();
-const port = 3001;
+const port = 3000;
 
 app.use(express.static('./client/dist'));
 app.use(Parser.urlencoded({extended: true}));
